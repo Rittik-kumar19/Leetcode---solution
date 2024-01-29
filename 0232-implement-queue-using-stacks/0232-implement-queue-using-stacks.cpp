867f903561f35c3515f7c9612cbf5e53933cbd50
Rittik kumar <76815180+Rittik-kumar19@users.noreply.github.com>
2024-01-29 20:04:39 +0530

    Time: 0 ms (100.00%), Space: 8.5 MB (5.23%) - LeetHub

---
+++ 0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.cpp
@@ -0,0 +1,47 @@
+class MyQueue {
+public:
+    stack<int>input,output;
+    MyQueue() {
+    }
+    void push(int x) {
+        if(input.size()>0){
+            while(input.size()>0){
+                int p=input.top();
+                output.push(p);
+                input.pop();
+            }
+        }
+        input.push(x);
+        if(output.size()>0){
+            while(output.size()>0){
+                int pp=output.top();
+                input.push(pp);
+                output.pop();
+            }
+        }
+    }
+    int pop() {
+        if(input.size()>0){
+            int y=input.top();
+            input.pop();
+            return y;
+        }
+        return -1;
+    }
+    
+    int peek() {
+        if(input.size()>0){
+            int z=input.top();
+            return z;
+        }
+        return -1;
+    }
+    bool empty() {
+        if(input.size()==0){
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+};
